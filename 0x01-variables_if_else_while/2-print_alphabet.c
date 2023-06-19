4f206dec1bc7cac8fe115fca4146d02b33f57a11
Vanessa <esslynnessa@gmail.com>
2023-06-18 17:38:25 -0700

    alx programming

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entery point
+ *Return: always 0
+ */
+int main(void)
+{
+	char al = 'a';
+
+	while (al <= 'z')
+	{
+		putchar(al);
+		al++;
+	}
+		putchar('\n');
+	return (0);
+}
+
